2e188cb3aca3595e008477f3fcc41a572216e33e
khazhimatov <khazhimatov959@gmail.com>
2023-05-13 23:40:31 +0300

    add paramsSerializer

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,9 +1,13 @@
 import axios from "axios";
+import qs from "qs"
 const settings = require('./settings.json');
 
 const fetcher = axios.create({
   ...axios.defaults,
   baseURL: settings.APIUrl,
+  paramsSerializer: (params) => {
+    return qs.stringify(params, {arrayFormat: "repeat"})
+  }
 });
 
 export default fetcher
